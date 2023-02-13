c869a5f39a1661a26c44ddbf3eea85ae4a6af1cb
antoniofilazzola-rva <121103418+antoniofilazzola-rva@users.noreply.github.com>
2023-02-13 23:20:00 +0100

    capitolo 3 prima parte

---
+++ 03/demos/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -0,0 +1,39 @@
+﻿using WarehouseManagementSystem.Domain;
+
+namespace WarehouseManagementSystem.Business
+{
+    public class OrderProcessor
+    {
+
+        //Dichiarazione del delegato
+        public delegate void OrderInitialized(Order order);
+        public delegate void ProcessCompleted();
+
+        //Esposizione del delegato come public setter e public getter
+
+        /*This property could now point to any method that corresponds with the method
+
+        signature that we defined when declaring the delegate
+        */
+        public OrderInitialized OnOrderInitialized { get; set; }
+        private void Initialize(Order order)
+        {
+            ArgumentNullException.ThrowIfNull(order);
+
+            //OnOrderInitialized();
+
+            OnOrderInitialized?.Invoke(order);
+            
+        }
+
+        public void Process(Order order, ProcessCompleted onCompleted=default)
+        {
+            // Run some code..
+
+            Initialize(order);
+            onCompleted?.Invoke();
+
+            // How do I produce a shipping label?
+        }
+    }
+}
